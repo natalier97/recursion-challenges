bc9f7419f63ca6c4a9137207cf16b776ec260bd7
Natalie <rivero_natalie@outlook.com>
2024-02-16 15:10:26 -0500

    finished factorial

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -1,5 +1,13 @@
+""" a factorial is the result of multiplying a sequence of descending numbers down to 0. Factorials are only defined for non-negative integer numbers. This definition includes zero. Though 0! is equal to 1, so treat it as an edge case."""
 def factorial(x):
-	pass
+	if x == 0:
+		return 1
+	total = x * factorial(x-1)
+	return total
+
+
+
+
 
 def palindrome(string):
 	pass
--- python/recursion_spec.py
+++ python/recursion_spec.py
@@ -1,1 +1,11 @@
 # Write your unit tests here
+from recursion_challenge import factorial, palindrome
+
+
+##---------------------------factorial tests
+
+#base case test - (0)
+print(f"should == 1:  {factorial(0)}")
+
+#should == 24
+print(f"should == 24:  {factorial(4)}")
