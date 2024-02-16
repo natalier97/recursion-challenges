830c9d66b21cc4e33dfcbd42c48d3329167dd05a
Natalie <rivero_natalie@outlook.com>
2024-02-16 16:50:47 -0500

    completed bottles, palindrome

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -7,13 +7,61 @@ def factorial(x):
 
 
 
-
+'''A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward, such as madam or kayak. --> returns True or False if string is a palindrome'''
 
 def palindrome(string):
-	pass
+	# last_index = len(string) - 1
+	# i = last_index
+	# reverse_string = ''
+	# while i > - 1:
+	# 	reverse_string += string[i] 
+	# 	i -= 1
+	# if reverse_string == string:
+	# 	return True
+	# else:
+	# 	return False
+
+	length = len(string)
+	if length == 1:
+		return True
+	if string[0] != string[-1]:
+		return False
+	return palindrome(string[1:-1])
+
+
+	# length = len(string)
+	# i = length
+	# reverse_string = ''
+	# if len(string) <= 1: #base case bc if len of string = 1 (aka last leter in str) --> 1-1 = 0 so -1 means we dont have any other letters to add to reversed string
+	# 	return reverse_string
+	# reverse_string = string[-1] + palindrome(string[:(i -1)])
+	# print(reverse_string)
+
+
+
+
+'''92 bottles of beer on the wall, 92 bottles of beer.
+Take one down and pass it around, 91 bottles of beer on the wall.
+91 bottles of beer on the wall, 91 bottles of beer.
+Take one down and pass it around, 90 bottles of beer on the wall.
+...
+...
+...
+Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.'''
 
 def bottles(num):
-	pass
+	bottle_plural = 'bottles' if num > 1 else "bottle"
+	str = f"""{num} {bottle_plural} of beer on the wall, {num} {bottle_plural} of beer. 
+				Take one down, pass it around, {num-1} bottles of beer on the wall."""
+	if num == 1:
+		return str
+	return str, bottles(num-1)
+	
+
 
 def roman_num(num):
 	pass
--- python/recursion_spec.py
+++ python/recursion_spec.py
@@ -1,11 +1,26 @@
 # Write your unit tests here
-from recursion_challenge import factorial, palindrome
+from recursion_challenge import factorial, palindrome, bottles
 
 
 ##---------------------------factorial tests
 
-#base case test - (0)
-print(f"should == 1:  {factorial(0)}")
+# #base case test - (0)
+# print(f"should == 1:  {factorial(0)}")
 
-#should == 24
-print(f"should == 24:  {factorial(4)}")
+# #should == 24
+# print(f"should == 24:  {factorial(4)}")
+
+
+##---------------------------palindrome tests
+# print(f"should == True:   {palindrome('racecar')}")
+
+# print(f"should == True:   {palindrome('ciVic')}")
+
+# print(f"should == False:   {palindrome('nice')}")
+
+# print(f"should == False:   {palindrome('bomb')}")
+
+
+
+##---------------------------bottles tests
+# print(f"THE SONG:\n   {bottles(5)}")
